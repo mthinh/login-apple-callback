dd46c23e08e1ac796ab4a5c2acbd0ffb89cda2ea
thinhcp <thinh@coderpush.com>
2025-08-02 16:59:36 +0700

    chore: add comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const BUNDLE_IDENTIFIER = "social.lemonade.app.staging";
+const BUNDLE_IDENTIFIER = "social.lemonade.app.staging"; // Prod: social.lemonade.app
 
 // Middleware to parse URL-encoded bodies (Apple sends form data)
 app.use(express.urlencoded({ extended: true }));
